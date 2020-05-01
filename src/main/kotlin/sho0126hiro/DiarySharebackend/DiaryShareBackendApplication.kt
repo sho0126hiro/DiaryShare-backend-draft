90099cd23a666555682e99578c01d8af395dd2e5
sho0126hiro <sho0126hiro@gmail.com>
2020-05-02 02:10:51 +0900

    create project

---
+++ src/main/kotlin/sho0126hiro/DiarySharebackend/DiaryShareBackendApplication.kt
@@ -0,0 +1,11 @@
+package sho0126hiro.DiarySharebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiaryShareBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiaryShareBackendApplication>(*args)
+}
---
+++ src/test/kotlin/sho0126hiro/DiarySharebackend/DiaryShareBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package sho0126hiro.DiarySharebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiaryShareBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
